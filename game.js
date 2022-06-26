0f5dc29d79a458610c9b78bb8fa78bcc7363090c
Zahar <91004012+zaharsm@users.noreply.github.com>
2022-06-26 18:04:32 +0530

    Update

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).keypress(function() {
+$(document).on("keypress touchstart" function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
